026504236c827a3471d3567c8eff42a58ce9dfa1
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-01-04 05:17:42 +0100

    avatrn: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: motorola edge 2024 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_avatrn.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_avatrn-user \
+    omni_avatrn-userdebug \
+    omni_avatrn-eng
---
+++ omni_avatrn.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from avatrn device
+$(call inherit-product, device/motorola/avatrn/device.mk)
+
+PRODUCT_DEVICE := avatrn
+PRODUCT_NAME := omni_avatrn
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := motorola edge 2024
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="avatrn-user 12 U2UANS34.72-43-2-6 0a3ef release-keys"
+
+BUILD_FINGERPRINT := motorola/avatrn/avatrn:12/U2UANS34.72-43-2-6/0a3ef:user/release-keys
